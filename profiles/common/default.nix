9a94aeebbeaba8104aff8111a58496f6728f8cad
Erin van der Veen <erin@erinvanderveen.nl>
2023-04-30 19:36:28 +0200

    common: add i18n configuration

--- profiles/common/default.nix
+++ profiles/common/default.nix
@@ -112,6 +112,21 @@
     mutableUsers = true;
   };
 
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "nl_NL.UTF-8";
+      LC_IDENTIFICATION = "nl_NL.UTF-8";
+      LC_MEASUREMENT = "nl_NL.UTF-8";
+      LC_MONETARY = "nl_NL.UTF-8";
+      LC_NAME = "nl_NL.UTF-8";
+      LC_NUMERIC = "nl_NL.UTF-8";
+      LC_PAPER = "nl_NL.UTF-8";
+      LC_TELEPHONE = "nl_NL.UTF-8";
+      LC_TIME = "nl_NL.UTF-8";
+    };
+  };
+
   # Sound
   sound.enable = true;
   # rtkit allows pipewire to get real time scheduling on demand (recommended by nixos wiki)
