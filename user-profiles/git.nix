5bf1a87bad346b38a8c80e5614677b9cfb061dd7
Erin van der Veen <erin@erinvanderveen.nl>
2023-08-01 10:13:04 +0200

    git: Add .cache to global gitignore

--- user-profiles/git.nix
+++ user-profiles/git.nix
@@ -10,15 +10,15 @@
 
     ignores = [
       "*~"
-      ".tags"
+      ".cache"
       ".direnv"
+      ".tags"
     ];
 
     extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = false;
     };
-
 
     includes = [
       {
@@ -55,12 +55,9 @@
       h1rd = "hard HEAD~1";
 
       # logging
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      plog =
-        "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
-      tlog =
-        "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      plog = "log --graph --pretty='format:%C(red)%d%C(reset) %C(yellow)%h%C(reset) %ar %C(green)%aN%C(reset) %s'";
+      tlog = "log --stat --since='1 Day Ago' --graph --pretty=oneline --abbrev-commit --date=relative";
       rank = "shortlog -sn --no-merges";
 
       # delete merged branches
