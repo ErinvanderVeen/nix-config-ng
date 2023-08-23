ccf035a8fea6c6620583e169276f668bc49b598c
Erin van der Veen <erin@erinvanderveen.nl>
2023-08-23 19:00:10 +0200

    Rename kyjan-packages to kyjan

--- user-profiles/kyjan.nix
+++ user-profiles/kyjan.nix
@@ -1,2 +1,11 @@
-{pkgs, ...}: {
+# This module contains the home manager configuration that is only intended for the "kyjan" user.
+{ pkgs, ... }:
+{
+  home = {
+    packages = with pkgs; [
+      mixxx
+      olive-editor
+      whatsapp-for-linux
+    ];
+  };
 }
