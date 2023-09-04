7862bc58dbcc74313ac6b551dfbe35167f26a924
Erin van der Veen <erin@erinvanderveen.nl>
2023-09-04 09:28:29 +0200

    kyjan: add pitivi as possible replacement for olive

--- user-profiles/kyjan.nix
+++ user-profiles/kyjan.nix
@@ -1,10 +1,10 @@
 # This module contains the home manager configuration that is only intended for the "kyjan" user.
-{ pkgs, ... }:
-{
+{pkgs, ...}: {
   home = {
     packages = with pkgs; [
       mixxx
-      olive-editor
+      olive-editor # Support is probably ending soon (as of 2023-09-04)
+      pitivi # Possible replacement for olive
       whatsapp-for-linux
     ];
   };
