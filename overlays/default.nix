170d990e86a43efb273842bb66e7b309c8bd1a92
Erin van der Veen <erin@erinvanderveen.nl>
2024-01-31 09:41:06 +0100

    protonvpn-gui: overlay
    The new client doesn't seem to have been backported to 23.11

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,7 @@
 # This file defines overlays, the attributes from this file are added to the overlays in the flake.nix file.
-{inputs, ...}: {
+{ inputs, ... }: {
   # This one brings our custom packages from the 'pkgs' directory
-  additions = final: _prev: import ../pkgs {pkgs = final;};
+  additions = final: _prev: import ../pkgs { pkgs = final; };
 
   modifications = final: prev: {
     # These packages, we want to get from unstable
@@ -11,6 +11,7 @@
       discord
       element-desktop
       helix
+      protonvpn-gui
       ;
   };
 
