d4dc0421fb0e19869d687edab99bfcfe20803977
Erin van der Veen <erin@erinvanderveen.nl>
2024-02-08 13:20:19 +0100

    home-assitant: add tplink

--- profiles/home-assistant.nix
+++ profiles/home-assistant.nix
@@ -1,4 +1,4 @@
-{...}: {
+{ ... }: {
   services.home-assistant = {
     enable = true;
     openFirewall = true;
@@ -7,11 +7,14 @@
       "esphome"
       "met"
       "radio_browser"
+
+      # Tapo smart plug
+      "tplink"
     ];
     config = {
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
+      default_config = { };
     };
   };
 }
