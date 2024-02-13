dfb068b5a874d5162a59537158ad5ef1f6815357
Erin van der Veen <erin@erinvanderveen.nl>
2024-02-13 21:27:58 +0100

    home-assistant: add tosti automation

--- profiles/home-assistant.nix
+++ profiles/home-assistant.nix
@@ -17,6 +17,41 @@
       # Includes dependencies for a basic setup
       # https://www.home-assistant.io/integrations/default_config/
       default_config = { };
+      homeassistant = {
+        language = "en";
+      };
+      automation = [
+        {
+          description = "Zet tosti-aparaat uit na 8 minuten na aanzetten.";
+          mode = "single";
+          trigger = [
+            {
+              platform = "device";
+              type = "turned_on";
+              device_id = "d3ef241b23e991a74138344ab20a738f";
+              entity_id = "switch.tosti";
+              domain = "switch";
+              for = {
+                hours = 0;
+                minutes = 8;
+                seconds = 0;
+              };
+            }
+          ];
+          action = [
+            {
+              type = "turn_off";
+              device_id = "d3ef241b23e991a74138344ab20a738f";
+              entity_id = "switch.tosti";
+              domain = "switch";
+            }
+            {
+              service = "notify.mobile_app_fp3";
+              data.message = "Tosti is klaar!";
+            }
+          ];
+        }
+      ];
     };
   };
 }
