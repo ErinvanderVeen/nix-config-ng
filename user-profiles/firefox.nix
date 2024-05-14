4c0709628652ba8c71f684e86ba42e71cbfc5322
Erin van der Veen <erin@erinvanderveen.nl>
2024-05-14 08:40:37 +0200

    flake: update

--- user-profiles/firefox.nix
+++ user-profiles/firefox.nix
@@ -8,7 +8,6 @@
         "widget.gtk.rounded-bottom-corners.enabled" = true;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "svg.context-properties.content.enabled" = true;
-        # "gnomeTheme.hideSingleTab" = true;
         "gnomeTheme.bookmarksToolbarUnderTabs" = true;
         "gnomeTheme.normalWidthTabs" = false;
         "gnomeTheme.tabsAsHeaderbar" = false;
