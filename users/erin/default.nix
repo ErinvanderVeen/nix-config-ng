4c11af5ff7d5bf406f12099448b4990a18d8d3eb
Erin van der Veen <erin@erinvanderveen.nl>
2024-05-18 19:59:13 +0200

    erin: add Tequatl ssh key

--- users/erin/default.nix
+++ users/erin/default.nix
@@ -1,13 +1,13 @@
-{...}: {
+{ ... }: {
   users.users.erin = {
     name = "erin";
     description = "Erin van der Veen";
     initialHashedPassword = "$y$j9T$xMVMs3Pdccm9jI1CUV4Be0$ruxg5.m4KHPS3OxQ6X/s6jKgmXx5ZBMsydbGjUIX05/";
     home = "/home/erin";
     isNormalUser = true;
-    extraGroups = ["wheel" "networkmanager"];
+    extraGroups = [ "wheel" "networkmanager" ];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF8EiA/6R8FrrXYbhV0eIWqSv5svnXbuDr4l/qgDUQcO erin@erinvanderveen.nl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEHKbHnZJJ9fcqMEZ5hZzVTxzHgcDs4tJpCPYZx0XAij erin@Tequatl"
     ];
   };
 
