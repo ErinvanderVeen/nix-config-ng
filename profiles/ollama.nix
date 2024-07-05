7c8e57b2b0dc72efe5a34454b2755cce49613058
Erin van der Veen <erin@erinvanderveen.nl>
2024-07-05 08:57:07 +0200

    ollama: add nextjs

--- profiles/ollama.nix
+++ profiles/ollama.nix
@@ -1,9 +1,13 @@
 { ... }: {
   services.ollama = {
     enable = true;
-    acceleration = "rocm";
     host = "0.0.0.0";
     rocmOverrideGfx = "10.3.0";
     openFirewall = true;
+  };
+
+  services.nextjs-ollama-llm-ui = {
+    enable = true;
+    ollamaUrl = "http://127.0.0.1:11434";
   };
 }
