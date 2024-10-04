6feecc6c80a4af30d9c6645996e1eff4374a62cb
Erin van der Veen <erin@erinvanderveen.nl>
2024-10-04 13:04:33 +0200

    users: keys

--- users/nixos/default.nix
+++ users/nixos/default.nix
@@ -1,12 +1,11 @@
-{...}: {
+{ ... }: {
   users.users.nixos = {
     description = "default";
     initialHashedPassword = "$y$j9T$YL05DpRpKY0L9JEbeUvvu/$zdCZDf9iDJ0B3T9n2y7fKoIyHQhhB981Dcdff19Kqf9";
     isNormalUser = true;
-    extraGroups = ["wheel" "media"];
+    extraGroups = [ "wheel" "media" ];
     openssh.authorizedKeys.keys = [
-      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBD9ABNVaBX71Hb/u2IVOr9ZAAhnp7szmE3k6fX0FBPVTmoPMbSbewWdnsvi0syzIb+oWajU4ipHG5UZHkAMpIi8= erin@Gwen"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+jQ+XfIiLyP9y4GPoPCVqwgry+v6pi/nAma3z6oRqA erin@Tequatl"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIER8TSugkBgvunwpyhvEhu65kkidfoUoq5Euup0ehuu/ erin@Trahearne"
     ];
   };
 
