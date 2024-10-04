f8d69c79a53226a79aed62f0501887f21b10b424
Erin van der Veen <erin@erinvanderveen.nl>
2024-10-04 12:57:35 +0200

    mordremoth: update

--- profiles/samba.nix
+++ profiles/samba.nix
@@ -1,9 +1,11 @@
-{...}: {
+{ ... }: {
   services.samba = {
     enable = true;
-    extraConfig = ''
-      hosts allow = 192.168.0. 127.0.0.1 localhost
-    '';
+    settings = {
+      global = {
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+      };
+    };
     openFirewall = true;
   };
 }
