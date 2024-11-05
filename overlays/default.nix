2b25ecd00900a4c23bbf992ea34f2472be6bcfaa
Erin van der Veen <erin@erinvanderveen.nl>
2024-11-05 16:39:19 +0100

    gnome: several changes

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,7 +1,14 @@
 # This file defines overlays, the attributes from this file are added to the overlays in the flake.nix file.
-{ inputs, ... }: {
+{ ... }: {
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs { pkgs = final; };
 
-  modifications = final: prev: { };
+  modifications = final: prev: {
+    gnome-system-monitor = prev.gnome-system-monitor.overrideAttrs (_: _: {
+      postInstall = ''
+        rm $out/bin/gnome-system-monitor
+        ln -s ${final.mission-center}/bin/missioncenter $out/bin/gnome-system-monitor
+      '';
+    });
+  };
 }
--- user-profiles/gnome.nix
+++ user-profiles/gnome.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 {
+  home.packages = with pkgs.gnome; [
+  ] ++
+  (with pkgs; [
+    geary
+    gnome-contacts
+    evolution-data-server
+    gnome-system-monitor
+  ]);
+
   programs.gnome-shell = {
     enable = true;
     extensions = with pkgs.gnomeExtensions; [
