0c22c5563f1e0dbe3f564186c5b240958590d868
Erin van der Veen <erin@erinvanderveen.nl>
2024-12-13 21:16:01 +0100

    nextcloud: recognize

--- profiles/nextcloud.nix
+++ profiles/nextcloud.nix
@@ -21,6 +21,30 @@
     };
   };
 
+  # All required for recognize
+  environment.systemPackages = with pkgs; [
+    gnumake # installation requirement
+    nodejs_20 # runtime and installation requirement
+    nodejs_20.pkgs.node-pre-gyp # installation requirement
+    python3 # requirement for node-pre-gyp otherwise fails with exit code 236
+    util-linux # runtime requirement for taskset
+  ];
+
+  systemd.services.nextcloud-setup = {
+    script = ''
+      export PATH=$PATH:/etc/profiles/per-user/nextcloud/bin:/run/current-system/sw/bin
+
+      if [[ ! -e ${config.services.nextcloud.home}/store-apps/recognize/node_modules/@tensorflow/tfjs-node/lib/napi-v8/tfjs_binding.node ]]; then
+        if [[ -d ${config.services.nextcloud.home}/store-apps/recognize/node_modules/ ]]; then
+          cd ${config.services.nextcloud.home}/store-apps/recognize/node_modules/
+          npm rebuild @tensorflow/tfjs-node --build-addon-from-source
+        fi
+      fi
+
+      ln -sf ${lib.getExe pkgs.nodejs_20} ${config.services.nextcloud.datadir}/store-apps/recognize/bin/node
+    '';
+  };
+
   networking.firewall.allowedTCPPorts = [
     80
     443
