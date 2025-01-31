726132c7761a13f64ddba022f807e95c514105f1
Erin van der Veen <erin@erinvanderveen.nl>
2025-01-31 21:10:29 +0100

    lutris: remove old packages

--- user-profiles/desktop/gaming/lutris.nix
+++ user-profiles/desktop/gaming/lutris.nix
@@ -2,15 +2,5 @@
 {
   home.packages = with pkgs; [
     lutris
-    # For installing an managing proton
-    protonup
-
-    # For some packages
-    wineWowPackages.stable
-    winetricks
-    mono
-    dxvk
-    wget
-    appimage-run
   ];
 }
